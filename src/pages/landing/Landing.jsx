c9d08dc22850712668afde0edb4a686942a06a23
Acin639 <titusnjeru48@gmail.com>
2025-12-29 13:08:15 +0300

    Update Landing.jsx

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,42 +1,53 @@
 import React from 'react';
 import './Landing.css';
-import { useNavigate, useParams } from 'react-router-dom'
-import logo from '../../assets/normal_logo.png';
+import { useNavigate } from 'react-router-dom';
 
 const Landing = () => {
-  
-  //const {id} = useParams();
-  const navigate = useNavigate()
-  return (
-   <div className="hero">
-     
+  const navigate = useNavigate();
 
+  return (
+    <div className="hero">
       <header className="header">
-        <h1 className="logo" >LONESTREAM</h1>
+        <h1 className="logo">LONESTREAM</h1>
         <div className="controls">
           <select className="lang" aria-label="language">
             <option>English</option>
             <option>Français</option>
           </select>
-          <button className="sign" onClick={()=> navigate("/login")}>Sign In</button>
+          <button className="sign" onClick={() => navigate('/login')}>Sign In</button>
         </div>
       </header>
 
       <div className="center">
         <h1 className="title">Unlimited movies, TV shows and more</h1>
         <p className="subtitle">Watch anywhere. Cancel anytime.</p>
-        <p className="subtitle small">Ready to watch? Enter your email to create or restart your membership.</p>
+        <p className="subtitle small">
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
 
-        <form className="emailForm" onSubmit={(e)=>{e.preventDefault(); alert('Get Started clicked') }}>
+        <form
+          className="emailForm"
+          onSubmit={(e) => {
+            e.preventDefault();
+            alert('Get Started clicked');
+          }}
+        >
           <input className="email" type="email" placeholder="Email Address" required />
-          <button className="cta" type="submit" onClick={() => navigate("/home")}
-           >Get Started ▸</button>
+          <button
+            className="cta"
+            type="submit"
+            onClick={() => navigate('/home')}
+          >
+            Get Started ▸
+          </button>
         </form>
       </div>
 
-      <footer className="attribution">This is a UI and Other 3ngines are built by — GENSYS™️</footer>
+      <footer className="attribution">
+        This is a UI and Other 3ngines are built by — GENSYS™️
+      </footer>
     </div>
   );
-}
+};
 
 export default Landing;
