6314c3bc7c1ef4438fa56c6ec9b98df65d08707d
Acin639 <titusnjeru48@gmail.com>
2025-12-29 13:12:48 +0300

    Fix import statement for arrow image in Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import "./Login.css"
 import logo from "../../assets/logo.png"
-import arrow from "../../assets/arrow_left.png
+import arrow from "../../assets/arrow_left.png"
 import { login, signup } from '../../firebase'
 import netflix_spinner from "../../assets/netflix_spinner.gif"
 import {useNavigate} from 'react-router-dom'
